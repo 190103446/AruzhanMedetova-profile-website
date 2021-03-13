665f70ea2c06b431dcd7fc708da5db3ebd645b15
ARU <190103446@stu.sdu.edu.kz>
2021-03-14 00:45:18 +0600

    create blog index page

--- routes/web.php
+++ routes/web.php
@@ -5,6 +5,8 @@ use Illuminate\Support\Facades\DB;
 
 use App\Models\Post;
 
+use App\Http\Controllers\BlogController;
+
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -43,3 +45,5 @@ Route::get('post', function () {
     $post = Post::find(1);
     return $post; 
 }); 
+
+Route::get('blog', [BlogController::class, 'index']); 
